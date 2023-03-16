e3ed536d1df7f97bb6351e92f50a4b61793aacd7
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-16 17:27:16 +0200

    feat: init navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   console.log('');
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"><Navbar /></div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
