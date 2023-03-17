85f5b30b2dcd74dcad0ea70c03b4c00b4e8bee8c
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-17 09:03:15 +0200

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
@@ -7,7 +8,10 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"><Navbar /></div>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
       </div>
     </BrowserRouter>
   );
