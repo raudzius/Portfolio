1e8e4e50fde44e4646c55b10a2f6f39155b76651
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-20 16:00:18 +0200

    feat: make earth and stars models relative

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import About from './components/About';
+import StarsCanvas from './components/canvas/StarsCanvas';
+import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -20,6 +22,10 @@ const App: React.FC = () => {
         <Experience />
         <Tech />
         <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   );
