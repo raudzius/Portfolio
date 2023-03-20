9684b71f65a73501fc1c89a8bbafc47c6a0b3286
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-20 22:59:52 +0200

    chore: remove unnecessary usestate hook

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -3,9 +3,9 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import React from 'react';
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars: React.FC = (props) => {
+const Stars: React.FC = () => {
   const ref = React.useRef<any>(null);
-  const [sphere] = React.useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 }) as Float32Array;
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -14,7 +14,7 @@ const Stars: React.FC = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
         <PointMaterial
           transparent
           color="#f272c8"
