6a3799f5bdf992d4e8189759c1ba5649680e52d4
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-20 10:20:13 +0200

    chore: add dot after text in experience header

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -59,7 +59,7 @@ const Experience: React.FC = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Experience</h2>
+        <h2 className={styles.sectionHeadText}>Experience.</h2>
       </motion.div>
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
