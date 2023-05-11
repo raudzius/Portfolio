650ba5415f055fd5a0eac58e2483c6d7c1b8cd67
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-11 13:55:35 +0300

    feat: update about section text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -36,8 +36,8 @@ const About: React.FC = () => (
       variants={fadeIn('', '', 0.1, 1)}
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
     >
-      I&apos;m a skilled software developer with experience in TypeScript, JavaScript, C#, and
-      expertise in frameworks like React, Node.js, DOT.NET and Three.js.
+      I&apos;m a skilled software developer with experience in TypeScript, JavaScript, C# and
+      expertise in frameworks like React and Three.js.
     </motion.p>
     <div className="mt-20 flex flex-wrap gap-10">
       {services.map((service, index) => (
